0bd56a7d2b030d502d231d9ce3a528fc892b5c46
cocokkkkk <davidfrolov75@gmail.com>
2024-08-11 15:58:36 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+#1st program
+print(9 **0.5 * 5)
+#2nd program
+print(9.99 > 9.98)
+print(1000 != 1001)
+#3rd program
+print(2 * 2 + 2)
+print(2 * (2 + 2))
+print((2 * 2 + 2) == (2 * (2 + 2)))
+#4th program
+x = '123.456'
+y = float(x) * 10
+print(int(y) % 10)
